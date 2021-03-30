e9b53785b5c1d6e2c9ff182501a228a06f7a5667
Allan Leal <allanjohnson.leal@gmail.com>
2021-03-29 21:46:00 -0300

    created the project leyout

--- index.js
+++ index.js
@@ -6,28 +6,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
 
+app.get('/question', (req, res)=>{
+    res.render('question', {})
+})
+
 app.get('/', (req, res)=>{
-
-    const welcome = true
-    const person = {
-        name: "Allan Johnson", 
-        office: "Sofware Engineer",
-        years: 28,
-        adress:{
-            street: "Rua canela quadra B7",
-            number: 17,
-            cep: 53370440
-        },
-        level: 12
-    }
-
-    res.render('index', {
-        name: person.name,
-        ofc: person.office,
-        year: person.years,
-        level: person.level,
-        message: welcome
-    })
+    res.render('answers', {})
 })
 
 app.listen(port, (error)=>{
