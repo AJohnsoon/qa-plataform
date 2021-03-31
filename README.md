f67c7c82033399ad42ba77eff5bb34baa50cbcc0
Allan Leal <allanjohnson.leal@gmail.com>
2021-03-30 22:08:57 -0300

    saving data past the forms and turning them into JSON

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Q&A
 ## Question and Answer Platform 
+
+- application currently creates the question and stores it in a URL in JavaScript format 
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+//work to json (not in use)
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res)=>{  
+    res.render('answers', {})
+})
 
 app.get('/question', (req, res)=>{
     res.render('question', {})
 })
 
-app.get('/', (req, res)=>{
-    res.render('answers', {})
+//route that saves form data using body-parser 
+app.post('/saveQuestions', (req, res)=>{
+    const question = {
+        titleQuestion :req.body.title,
+        descriptionQuestion : req.body.description
+    }
+
+    res.send(question)
 })
 
 app.listen(port, (error)=>{
