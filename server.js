9cffdf13526ed96c2e4ca8f69848e89daac3f770
Allan Leal <allanjohnson.leal@gmail.com>
2021-04-07 09:30:45 -0300

    refactor: delegate responsabilities to the file

--- server.js
+++ server.js
@@ -1,35 +1,7 @@
-const bodyParser = require('body-parser');
 const server = require('./app')
-const port = 3000;
 
-server.set('view engine', 'ejs');
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
-
-
-server.get('/', (req, res) => {
-    res.render('answers', {});
-});
-
-server.get('/question', (req, res) => {
-    res.render('question', {});
-});
-
-server.post('/saveQuestions', (req, res) => {
-    const question = {
-        titleQuestion: req.body.title,
-        descriptionQuestion: req.body.description
-    }
-
-    res.send(question);
-});
-
-server.listen(port, (error) => {
-    if (error) {
+server.listen(3000, (error) => {
+    if (error)
         console.log(error);
-    } else {
-        console.log("server is running");
-    }
+    console.log("server is running");    
 });
-
-module.exports = server
