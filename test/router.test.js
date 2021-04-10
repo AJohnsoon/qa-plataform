711b877f6275546b504193e2133f397b20e5fb8b
Allan Leal <allanjohnson.leal@gmail.com>
2021-04-10 00:36:02 -0300

    test: router return

---
+++ test/router.test.js
@@ -0,0 +1,18 @@
+const routers = require('../routes');
+const testRoute = {
+    title: 'Main test title',
+    description: 'Main test Description',
+    status: 200
+}
+
+describe('exemple router', ()=>{
+    test('should a router return status 200', async()=>{
+       
+        routers.get('/', (req, res)=>{                   
+            res.send(testRoute.description)
+        })
+        
+        expect(testRoute.description).toBe('Main test Description')
+    
+    })
+})
