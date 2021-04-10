12070e36869521ce4b7f20eed6529041cad9166e
Allan Leal <allanjohnson.leal@gmail.com>
2021-04-10 00:40:14 -0300

    refactor: tranferred questionModel to router

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const app = require('./routes');
 const connection = require('./database/data');
-const questionModel = require('./database/Question');
 
 connection.authenticate().then(() => {
         console.log('Connection success with (QA) database')
@@ -8,5 +7,6 @@ connection.authenticate().then(() => {
     .catch((err) => {
         console.log(err, 'error to connecting database')
     })
+    
 
 module.exports = app;
